9d565dd8779613bc58ad4fe3d4df31f191c1a151
LU Hua <yukiluq@gmail.com>
2017-06-21 22:42:14 +0800

    fix
    fix

--- redux/combineReducers.js
+++ redux/combineReducers.js
@@ -1,20 +1,3 @@
-/**
- * Turns an object whose values are different reducer functions, into a single
- * reducer function. It will call every child reducer, and gather their results
- * into a single state object, whose keys correspond to the keys of the passed
- * reducer functions.
- *
- * @param {Object} reducers An object whose values correspond to different
- * reducer functions that need to be combined into one. One handy way to obtain
- * it is to use ES6 `import * as reducers` syntax. The reducers may never return
- * undefined for any action. Instead, they should return their initial state
- * if the state passed to them was undefined, and the current state for any
- * unrecognized action.
- *
- * @returns {Function} A reducer function that invokes every reducer inside the
- * passed object, and builds a state object with the same shape.
- */
-
 //简化了代码，把错误处理都去除了
 
 export default function combineReducers(reducers) {
