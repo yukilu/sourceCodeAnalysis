a7531a5f716e80df71b5e6ba9145285df5bf3bc2
LU Hua <yukiluq@gmail.com>
2017-07-23 14:28:28 +0800

    thunk
    thunk

--- redux/redux-thunk.js
+++ redux/redux-thunk.js
@@ -19,10 +19,17 @@ function thunkMiddleware(store) {
         return function (action) {
             /*thunk函数加强了dispatch，使dispatch能处理函数，可以把异步逻辑封在函数内，从而是实现异步
             * 如dispatch(() => { setTimeout(() => { console.log(0); }, 1000) })
+            * 所以当传入dispatch时，就可以异步dipatch(action)
+            * dispatch(dispatch => { setTimeout(() => disptach(action), 1000); })
+            * 实际上就是异步时action为函数时，进入代码的action(dispatch)部分
+            * 然后在适当时候调用dispatch(action)，进入函数代码另一部分，当然中间件中写法为next(action)
+            */
+           /*
             * 此处当action为function时调用的是store.dispatch，而action为对象是调用的是next
             * 区别在于store.dispatch为最终加强的dispatch，包含thunk的功能，既能处理函数
             * 而next为前一middleware返回值，不包含当前thunk功能，即无法处理函数
-            * 若用next替换store.dispatch，则异步处理时，传入的dispatch不会包含thunk功能，造成无法处理函数，会出错*/
+            * 若用next替换store.dispatch，则异步处理时，传入的dispatch不会包含thunk功能，造成无法处理函数，会出错
+            */
             typeof action === 'function' ? action(store.dispatch, store.getState) : next(action);
         }
     }
