f234dc0ef9a02248c43e888026cc8c7bf9ddb7c6
LU Hua <yukiluq@gmail.com>
2017-07-30 17:17:19 +0800

    router
    router

---
+++ react-router/demo/RouteChildren.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+// Route中不存在component和render时，只有children
+// children为函数
+render(
+    <Router>
+        <div>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/a'>A</Link></li>
+            </ul>
+            <hr/>
+
+            <Route exact path='/' render={props => <h2>Home</h2>} />
+            <Route path='/a' children={props => <p>A</p>} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
+
+// children为Route子组件 <Route><A/></Route>
+function A(props) {
+    return <p>A</p>;
+}
+
+render(
+    <Router>
+        <div>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/a'>A</Link></li>
+            </ul>
+            <hr/>
+
+            <Route exact path='/' render={props => <h2>Home</h2>} />
+            <Route path='/a'><A/></Route>
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
---
+++ redux/demo/reduxConnectedRest.jsx
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+/* 用redux管理count，实现了一个简单的Counter，并向ConnectedCounter传入了name属性，有效
+ * 这个例子说明向connected的组件传递属性，最后可以到达原组件内，与向原组件传属性的效果相同(实际是渲染原组件时，将接收的属性传入)
+ * 应该是connect时，render代码大概为
+ * render() {
+ *   const { propA, propB, ...rest } = this.props;
+ *   return <wrapped propA={propA} propB={propB} {...rest} />
+ * }
+ */ 
+const actions = {
+    increase: () => ({ type: 'INCREASE' })
+};
+
+function reducer(state = { count: 0 }, action) {
+    const count = state.count;
+    switch (action.type) {
+        case 'INCREASE':
+            return { count: count + 1 };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+function Counter(props) {
+    const { count, name, increase } = props;
+    return(
+        <div>
+            <h3>Counter{name}</h3>
+            <p>{count}</p>
+            <button onClick={increase}>+</button>
+        </div>
+    );
+}
+
+function mapStateToProps(state) {
+    return { count: state.count };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        increase: () => dispatch(actions.increase())
+    };
+}
+
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+    <Provider store={store}>
+        <ConnectedCounter name='007' />
+    </Provider>,
+    document.getElementById('root')
+);
